0f306ed5e52c844f834772f4ff05737a84d607d2
balde ousmane <baldemarc225@gmail.com>
2023-07-03 13:45:06 +0100

    rendu de la page interactive

--- pieces.js
+++ pieces.js
@@ -1,12 +1,13 @@
 const reponse = await fetch("pieces-autos.json");
 const tabPieces = await reponse.json(); //extraire les element de json
 
-for(let i=0 ; i<5 ; i++){
+function genererPiece(listePiece){
+    
+for(let i=0 ; i<listePiece.length ; i++){
     //chaque objet est representer par article
-    const article = tabPieces[i];
+    const article = listePiece[i];
 
     //creation des balises
-
     const imageElement = document.createElement("img");
     imageElement.src = article.image;
     
@@ -25,11 +26,9 @@ for(let i=0 ; i<5 ; i++){
     const stockElement = document.createElement("p");
     stockElement.innerText = article.disponibilite ? "En stock" : "En rupture de stock";
 
-   
     //on attaches la balise article a la section fiches
     const sectionFiches = document.querySelector(".fiches");
     const pieceElement = document.createElement("article");
-
     sectionFiches.appendChild(pieceElement);
     
     pieceElement.appendChild(imageElement);
@@ -37,17 +36,22 @@ for(let i=0 ; i<5 ; i++){
     pieceElement.appendChild(prixElement);
     pieceElement.appendChild(categorieElement);
     pieceElement.appendChild(stockElement);
+    }
 }
+//appelle de la fonction
+genererPiece(tabPieces);
 
 //button pour trier les prix par ordre croissant
+
    const buttonTrier = document.querySelector(".btn-trier");
     buttonTrier.addEventListener("click", function (){
-        const copyTablePiece = Array.from(tabPieces); //on fais une copie de de tabPieces pour ne rien melanger;
-
-        copyTablePiece.sort(function(a,b){
+        const copyTableTrier = Array.from(tabPieces); //on fais une copie de de tabPieces pour ne rien melanger;
+        copyTableTrier.sort(function(a,b){
             return a.prix - b.prix ;
         });
-        console.log(copyTablePiece);
+        document.querySelector(".fiches").innerHTML="";
+        genererPiece(copyTableTrier);
+        
     });
 
     //button pour filtrer les prix abordables
@@ -57,18 +61,22 @@ for(let i=0 ; i<5 ; i++){
         const pieceFiltrees = tabPieces.filter(function(piece){
             return piece.prix <= 35 ;//retourne les prix non negociable
         });
-        console.log(pieceFiltrees);
+        document.querySelector('.fiches').innerHTML = "";
+        genererPiece(pieceFiltrees);
+        
     });
 
     //button pour trier les prix par ordre decroissant
     const buttonTrieDecroissant = document.querySelector(".btn-decroissant");
     buttonTrieDecroissant.addEventListener("click", ()=>{
-        const copieTablePiece = Array.from(tabPieces);
+        const copieTableDecroissant = Array.from(tabPieces);
 
-        copieTablePiece.sort(function (a,b){
+        copieTableDecroissant.sort(function (a,b){
             return b.prix - a.prix ;
         }); 
-        console.log(copieTablePiece);
+        document.querySelector('.fiches').innerHTML = "";
+        genererPiece(copieTableDecroissant);
+        
     });
 
     //filtrer(enlever) les pieces sans description
@@ -78,9 +86,22 @@ for(let i=0 ; i<5 ; i++){
         const pieceAvecDescription = tabPieces.filter(function(piece){
             return piece.description;
         });
-        console.log(pieceAvecDescription);
+        document.querySelector('.fiches').innerHTML = "";
+        genererPiece(pieceAvecDescription);
     });
 
+    //filtrer par prix maximum
+    let range = document.querySelector('#Prix-Max');
+    range.addEventListener("input",()=>{
+        const prixFiltres = tabPieces.filter(function(piece){
+            return piece.prix <= range.value;
+        });
+        document.querySelector('.fiches').innerHTML = "";
+        genererPiece(prixFiltres);
+         
+    })
+
+    // part 2
 
     //Affichage d'une liste des prix abordables
     const tabNom = tabPieces.map(pieces => pieces.nom);//fonction lambda
@@ -112,8 +133,6 @@ for(let i=0 ; i<5 ; i++){
             tabNomDisponible.splice(i,1);
         }
     }
-    console.log(tabPrixDisponible)
-   
    
     const ull = document.createElement("ul")
    
